47c9f32703210ac9d71b1d7094b48def48bdc53d
ddenis61 <ddidenko20142014@yandex.ru>
2024-11-28 17:54:05 +0300

    remove_file

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import uvicorn
 from fastapi import FastAPI, Request
 import runpy
+import os
 
 
 app = FastAPI()
@@ -21,15 +22,17 @@ app = FastAPI()
 def getall(request: Request):
     req = request.headers.get('code')
 
-    with open('code1.py', 'w', encoding='utf8') as f:
-        f.write(req)
     try:
+        with open('code1.py', 'w', encoding='utf8') as f:
+            f.write(req)
         runpy.run_module(mod_name="code1")
-    except :
-        return TypeError
-    else:
+        os.remove('code1.py')
         return 200
+    except Exception as a:
+        return str(a)
+    else:
+        return '500'
 
 
 if __name__ == "__main__":
-    uvicorn.run('main:app', host='0.0.0.0', port=5658, reload=True)
+    uvicorn.run('main:app', host='0.0.0.0', port=5658)
